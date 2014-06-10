322a073d4227003e447d291a75ac7de7ed0ff716
courtneyjooste <courtney@2baobablogic.com>
2014-06-10 04:35:36 +0200

    Dynamic Headers

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,10 +9,9 @@ describe "Static pages" do
       expect(page).to have_content('Sample App')
     end
 
-    it "should have the title 'Home'" do
+    it "should have the base title" do
       visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
-    end
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Homr")
   end
 
   describe "Help page" do
@@ -37,7 +36,7 @@ describe "Static pages" do
 
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About us")
     end
   end
 end
