20ed72da445634e31431e470f9f5035568d0a596
CourtneyJooste <courtney@2baobablogic.com>
2014-06-12 10:28:34 +0200

    added user model validation and started adding password hashing

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -3,10 +3,10 @@ require 'rails_helper'
 RSpec.describe StaticPagesController, :type => :controller do
 
   describe "GET '...'" do
-    it "returns http success" do
-      get '...'
-      expect(response).to be_success
-    end
+    #it "returns http success" do
+      #get '...'
+      #expect(response).to be_success
+    #end
   end
 
 end
